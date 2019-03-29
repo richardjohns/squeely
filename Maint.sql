d05ada4c2dfef96a7138a7724c9bc92b3b8e5dd7
richardjohns <richard.johns193@gmail.com>
2019-03-29 13:57:29 +1300

    Update Maint.sql

--- Maint.sql
+++ Maint.sql
@@ -7,11 +7,19 @@ select
 	c.REQD_BY_DATEI AS 'Required by date',
 	t.TASK_NBRI AS 'Task number',
 	c.TASK_DESCR AS 'Task',
-	c.TASK_TYPE AS 'Task type',
+	CASE c.TASK_TYPE
+		WHEN 'O' THEN 'Operating'
+		ELSE c.TASK_TYPE
+	END AS 'Work type',	
 	ISNULL(k.ASSNBRI,'') AS 'Asset number',
 	ISNULL(s.DESCR,'') AS 'Asset description',
 	c.TASK_NARR AS 'Task narration',
+	u.UF_VAL06 AS 'Internal notes & comments',
 	c.TASK_DATA AS 'Completion comment',
+	CASE u.UF_VAL05
+		WHEN '1/01/1900' THEN ''
+		ELSE u.UF_VAL05
+	END AS 'Date PO generated',	
 	CASE c.TASK_STAGE
 		WHEN 'C' THEN 'Financially complete'
 		WHEN 'A' THEN 'Approved'
@@ -40,6 +48,7 @@ LEFT JOIN F1WRK_TASK_CTL_ASS k ON t.TASK_NBRI = k.TASK_NBRI
 LEFT JOIN F1WRK_TASK_LNE l ON t.TASK_NBRI = l.TASK_NBRI
 LEFT JOIN INF_CLG_PROD g ON l.SUP_PROD_NBR = g.prod_nbr
 LEFT JOIN F1ASR_REG_ASSET s ON k.ASSNBRI = s.ASSNBRI
+LEFT JOIN AMRPT_TASK_UF_VAL u ON t.TASK_NBRI = u.TASK_NBRI
 WHERE
 	c.TASK_STAGE <> 'R' 
 	AND
